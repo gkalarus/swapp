20238743a974c2d8c9fb5d9584b81c30fad210e8
Grzegorz <kalarus.g@gmail.com>
2019-03-01 14:06:32 +0100

    Updates

---
+++ precache-manifest.d8e21e4efe7329486e3a1fb1ac3720ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea78daa5f1630ba3f52b",
+    "url": "/swapp/static/css/main.56b0ef98.chunk.css"
+  },
+  {
+    "revision": "ea78daa5f1630ba3f52b",
+    "url": "/swapp/static/js/main.ea78daa5.chunk.js"
+  },
+  {
+    "revision": "08697d671f21e5316448",
+    "url": "/swapp/static/js/runtime~main.08697d67.js"
+  },
+  {
+    "revision": "99547a0e1c026c16811a",
+    "url": "/swapp/static/css/2.623ca083.chunk.css"
+  },
+  {
+    "revision": "99547a0e1c026c16811a",
+    "url": "/swapp/static/js/2.99547a0e.chunk.js"
+  },
+  {
+    "revision": "4d66597d8badf6a02257a0704fa622fa",
+    "url": "/swapp/static/media/ARROWOPEN.4d66597d.svg"
+  },
+  {
+    "revision": "9f3a719add9d3b8ad4f8d2918acb5059",
+    "url": "/swapp/static/media/ARROWCLOSE.9f3a719a.svg"
+  },
+  {
+    "revision": "8b6d494529d2a18c9a45450ed663dce9",
+    "url": "/swapp/static/media/LOGO.8b6d4945.svg"
+  },
+  {
+    "revision": "6ec4ea4908ae20a2593329c95c86f67b",
+    "url": "/swapp/index.html"
+  }
+];
